482f00c2e726d05a49ce47a9827b43ae06df4a09
Eelke van der Horst <eelkevanderhorst@gmail.com>
2013-10-30 08:41:31 -0700

    Added some methods.

--- src/main/java/nl/lumc/nanopub/store/dao/NanopubDao.java
+++ src/main/java/nl/lumc/nanopub/store/dao/NanopubDao.java
@@ -1,7 +1,18 @@
 package nl.lumc.nanopub.store.dao;
 
+import java.net.URI;
+import java.util.List;
+
+import ch.tkuhn.nanopub.Nanopub;
+
+
 public interface NanopubDao {
 	
+	public URI storeNanopub(Nanopub nanopub);
+	
+	public Nanopub retrieveNanopub(URI uri);
+	
+	public List<URI> listNanopubs();
 //	public Nanopub retrieveNanopub();
 
 }
