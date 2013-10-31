9c69e482cb7a5c3619256c781a504c88a5ef49e3
Reinout van Schouwen <reinout@gmail.com>
2013-10-31 16:19:00 +0100

    Remove rdf2go dependency, add Sesame dependencies

--- src/test/java/nl/lumc/nanopub/store/dao/impl/NanopubDaoImplTest.java
+++ src/test/java/nl/lumc/nanopub/store/dao/impl/NanopubDaoImplTest.java
@@ -1,5 +1,6 @@
 package nl.lumc.nanopub.store.dao.impl;
 
+import ch.tkuhn.nanopub.MalformedNanopubException;
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
@@ -11,43 +12,45 @@ import org.junit.Test;
 
 import ch.tkuhn.nanopub.Nanopub;
 import ch.tkuhn.nanopub.NanopubImpl;
+import java.io.IOException;
+import org.openrdf.OpenRDFException;
+import org.openrdf.rio.RDFFormat;
 
 @Ignore
 public class NanopubDaoImplTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testNanopubDaoImpl() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testNanopubDaoImpl() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testStoreNanopub() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testStoreNanopub() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testRetrieveNanopub() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testRetrieveNanopub() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testListNanopubs() {
-		fail("Not yet implemented");
-	}
-	
-	
-	private Nanopub getNanopubFixture() {
-		InputStream stream = this.getClass().getResourceAsStream("../../example.trig.rdf");
-		Nanopub nanopub = new NanopubImpl(in, format)
-            return null;
-	}
+    @Test
+    public void testListNanopubs() {
+        fail("Not yet implemented");
+    }
+
+    private Nanopub getNanopubFixture() throws MalformedNanopubException, OpenRDFException, IOException {
+        InputStream stream = this.getClass().getResourceAsStream("../../example.trig.rdf");
+        Nanopub nanopub = new NanopubImpl(stream, RDFFormat.TRIG);
+        return nanopub;
+    }
 
 }
