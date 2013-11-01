7a811df36fbf88f29836af4a3795950390e97924
Rajaram <rr.kaliyaperumal@gmail.com>
2013-11-01 09:48:41 +0100

    NanopubDaoImplTest test class modified, not working yet. Issues with Repository object.

--- src/test/java/nl/lumc/nanopub/store/dao/impl/NanopubDaoImplTest.java
+++ src/test/java/nl/lumc/nanopub/store/dao/impl/NanopubDaoImplTest.java
@@ -1,54 +1,75 @@
 package nl.lumc.nanopub.store.dao.impl;
 
+import ch.tkuhn.nanopub.MalformedNanopubException;
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import ch.tkuhn.nanopub.Nanopub;
 import ch.tkuhn.nanopub.NanopubImpl;
+import java.io.IOException;
+import java.util.List;
+import nl.lumc.nanopub.store.dao.NanopubDaoException;
+import org.openrdf.OpenRDFException;
+import org.openrdf.model.URI;
+import org.openrdf.repository.Repository;
+import org.openrdf.rio.RDFFormat;
 
 @Ignore
 public class NanopubDaoImplTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testNanopubDaoImpl() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testStoreNanopub() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRetrieveNanopub() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testListNanopubs() {
-		fail("Not yet implemented");
-	}
+    
+    private NanopubDaoImpl nanopubDaoImpl;
+    
+    private Repository repository;
 	
+    @Before	
+    public void setUp() throws Exception {
+        
+        repository = null;        
+        nanopubDaoImpl = new NanopubDaoImpl(repository);
 	
-	private Nanopub getNanopubFixture() {		
-            InputStream stream = this.getClass().getResourceAsStream("../../example.trig.rdf");
-            Nanopub nanopub = new NanopubImpl(in, format);            
+    }	
+    	
+    @Test	
+    public void testStoreNanopub() throws MalformedNanopubException,     
+            NanopubDaoException, OpenRDFException, IOException {
+        
+        URI uri = null;
+        uri = nanopubDaoImpl.storeNanopub(getNanopubFixture());        
+        assertNotNull(uri);	    
+    }
+	
+    @Test	
+    public void testRetrieveNanopub() throws MalformedNanopubException, 
+            OpenRDFException, IOException, NanopubDaoException {
+        
+        Nanopub np = null;
+        URI uri = getNanopubFixture().getUri();
+        np = nanopubDaoImpl.retrieveNanopub(uri);
+        assertNotNull(np);
+    }
+	
+    @Test	
+    public void testListNanopubs() throws NanopubDaoException {		
+        
+        List<URI> uris = null;
+        uris = nanopubDaoImpl.listNanopubs();
+        assertNotNull(uris);
+        
+    }
+    
+    private Nanopub getNanopubFixture() throws MalformedNanopubException,                 
+            OpenRDFException, IOException {            
             
-            return nanopub;
-	}
+        InputStream stream = this.getClass().                    
+                getResourceAsStream("../../example.trig.rdf");            
+        Nanopub nanopub = new NanopubImpl(stream, RDFFormat.TRIG);
+            
+        return nanopub;	
+    }
 
 }
