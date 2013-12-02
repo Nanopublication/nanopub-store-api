0fe1e975d72ccb5e2ee35090589633eabf98103c
Rajaram <rr.kaliyaperumal@gmail.com>
2013-12-02 14:26:52 +0100

    Enchanced NanopubControllerTest.
    Removed Context configuration, Mocked annotations

--- src/test/java/nl/lumc/nanopub/store/api/NanopubControllerTest.java
+++ src/test/java/nl/lumc/nanopub/store/api/NanopubControllerTest.java
@@ -3,41 +3,31 @@ package nl.lumc.nanopub.store.api;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-
 import java.io.IOException;
 import java.util.List;
-
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
-
 import nl.lumc.nanopub.store.api.json.ResponseWrapper;
 import nl.lumc.nanopub.store.utils.NanopublicationFileOperation;
 import nl.lumc.nanopub.store.dao.NanopubDao;
 import nl.lumc.nanopub.store.dao.NanopubDaoException;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openrdf.OpenRDFException;
-import org.openrdf.model.URI;
 import org.openrdf.model.impl.URIImpl;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
 import ch.tkuhn.nanopub.MalformedNanopubException;
 import ch.tkuhn.nanopub.Nanopub;
+import org.openrdf.model.URI;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
 
@@ -45,24 +35,22 @@ import org.mockito.Mock;
  *
  * @author Reinout, Rajaram, Eelke, Mark
  * @since 25-10-2013
- * @version 0.2
+ * @version 0.3
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration("classpath:test-api-context.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@WebAppConfiguration
+//@ContextConfiguration("classpath:test-api-context.xml")
 public class NanopubControllerTest {
     
 //    @Inject
 //    private WebApplicationContext wac;
 
     @Mock
-    private NanopubDao nanopubDao;
+    private NanopubDao nanopubDao;    
     
-    @Inject
     @InjectMocks
-    private NanopubController controller;
+    private NanopubController controller;    
     
-    //@Inject
     private NanopublicationFileOperation npFileOperation = 
             new NanopublicationFileOperation();
 
@@ -70,16 +58,23 @@ public class NanopubControllerTest {
 
     @Before
     public void setup() {
+        // Process mock annotations
+        MockitoAnnotations.initMocks(this);
       //mockMvc = webAppContextSetup(this.wac).build();
-        mockMvc = standaloneSetup(controller).build();
-        nanopubDao = mock(NanopubDao.class);        
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        //this.nanopubDao = mock(NanopubDao.class);        
 //        controller.setNanopubDao(nanopubDao); // should be injected by @InjectMocks
     }
     
 
     @Test
     public void testStoreNanopubURLMapping() throws Exception {
-        mockMvc.perform(get("/nanopubs/1")).andExpect(status().isOk()); //.param("np", "bla bla")); // /nanopubs/?np=bla%20bla
+        String id = "1";
+        //URI uri = new URIImpl(id);
+        //Nanopub nanopub = npFileOperation.getNanopubFixture(".."
+         //       + "/example.trig.rdf"); 
+        //when(nanopubDao.retrieveNanopub(uri)).thenReturn(nanopub);
+        mockMvc.perform(get("/nanopubs/"+id)).andExpect(status().isOk()); //.param("np", "bla bla")); // /nanopubs/?np=bla%20bla
     }
     
     @Test
